0e88f2e77850395ff8ec2c11710210175f283c41
Raman Verma <ramanverma.09@gmail.com>
2017-10-24 02:02:57 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
+import { RouterModule }   from '@angular/router';
 import { AppComponent } from './app.component';
+import { OneComponent } from './one.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, OneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularMultiSelectModule,
+    RouterModule.forRoot([
+  {
+    path: 'one',
+    component: OneComponent
+  }
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
