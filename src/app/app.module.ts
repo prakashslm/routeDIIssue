06e7c6e7e0f359e28e4e6061ff4a11b5c09f55a0
Prakash Vivekanandan <prakash.vivekanandan@qlis.com>
2017-10-24 21:00:02 +0530

    resolve angular2 multiselect dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
-import { RouterModule }   from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { OneComponent } from './one.component';
 
-
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularMultiSelectModule,
+    RouterModule.forRoot([
+      { path: 'one', component: OneComponent }
+    ]) // , { useHash: true, preloadingStrategy: PreloadAllModules, enableTracing: true } )
+  ],
   declarations: [
     AppComponent, OneComponent
   ],
-  imports: [
-    BrowserModule,
-    AngularMultiSelectModule,
-    RouterModule.forRoot([
-  {
-    path: 'one',
-    component: OneComponent
-  }
-])
-  ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/one.component.ts
+++ src/app/one.component.ts
@@ -1,46 +1,59 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AngularMultiSelect } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
 
 @Component({
   selector: 'one-root',
-  template: 'hiii <angular2-multiselect [data]="dropdownList" [(ngModel)]="selectedItems" [settings]="dropdownSettings" (onSelect)="onItemSelect($event)" (onDeSelect)="OnItemDeSelect($event)"></angular2-multiselect>'
+  template: `<div>
+    hiii
+    <angular2-multiselect [data]="dropdownList" [(ngModel)]="selectedItems" [settings]="dropdownSettings" (onSelect)="onItemSelect($event)" (onDeSelect)="OnItemDeSelect($event)"></angular2-multiselect>
+  </div>`
 })
 export class OneComponent implements OnInit {
-    dropdownList = [];
-    selectedItems = [];
-    dropdownSettings = {};
-    ngOnInit(){
-        this.dropdownList = [
-                              {"id":1,"itemName":"India"},
-                              {"id":2,"itemName":"Singapore"},
-                              {"id":3,"itemName":"Australia"},
-                              {"id":4,"itemName":"Canada"},
-                              {"id":5,"itemName":"South Korea"},
-                              {"id":6,"itemName":"Germany"},
-                              {"id":7,"itemName":"France"},
-                              {"id":8,"itemName":"Russia"},
-                              {"id":9,"itemName":"Italy"},
-                              {"id":10,"itemName":"Sweden"}
-                            ];
-        this.selectedItems = [
-                                {"id":2,"itemName":"Singapore"},
-                                {"id":3,"itemName":"Australia"},
-                                {"id":4,"itemName":"Canada"},
-                                {"id":5,"itemName":"South Korea"}
-                            ];
-        this.dropdownSettings = { 
-                                  singleSelection: false, 
-                                  text:"Select Countries",
-                                  selectAllText:'Select All',
-                                  unSelectAllText:'UnSelect All',
-                                  enableSearchFilter: true
-                                };            
-    }
-    onItemSelect(item){
-        console.log('Selected Item:');
-        console.log(item);
-    }
-    OnItemDeSelect(item){
-        console.log('De-Selected Item:');
-        console.log(item);
-    }
+  dropdownList = [];
+  selectedItems: any = [];
+  dropdownSettings = {};
+
+  @ViewChild(AngularMultiSelect) ams: AngularMultiSelect;
+
+  constructor() {
+    this.dropdownList = [
+      { id: 1, itemName: 'India' },
+      { id: 2, itemName: 'Singapore' },
+      { id: 3, itemName: 'Australia' },
+      { id: 4, itemName: 'Canada' },
+      { id: 5, itemName: 'South Korea' },
+      { id: 6, itemName: 'Germany' },
+      { id: 7, itemName: 'France' },
+      { id: 8, itemName: 'Russia' },
+      { id: 9, itemName: 'Italy' },
+      { id: 10, itemName: 'Sweden' }
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      text: 'Select Countries',
+      enableCheckAll: true,
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true
+    };
+  }
+
+  ngOnInit() {
+    this.selectedItems = [
+      { id: 2, itemName: 'Singapore' },
+      { id: 3, itemName: 'Australia' },
+      { id: 4, itemName: 'Canada' },
+      { id: 5, itemName: 'South Korea' }
+    ];
+  }
+
+  onItemSelect(item) {
+    console.log('Selected Item:');
+    console.log(item);
+  }
+
+  OnItemDeSelect(item) {
+    console.log('De-Selected Item:');
+    console.log(item);
+  }
 }
